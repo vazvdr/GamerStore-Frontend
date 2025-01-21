ee8aed1f4048cc58963dae09fa7224cc55da38ab
vazvdr <azevedo.vanderson@hotmail.com>
2025-01-21 20:18:13 -0300

    Sending changes on page

--- src/app/(pagina)/page.tsx
+++ src/app/(pagina)/page.tsx
@@ -3,8 +3,6 @@ import { useState, useEffect } from 'react'
 import ProdutoItem from '@/components/produto/ProdutoItem'
 import useProdutos from '@/data/hooks/useProdutos'
 import { IconArrowNarrowUp } from '@tabler/icons-react'
-import { ProvedorSessao } from '@/data/contexts/ContextoSessao'
-import { ProvedorUsuario } from '@/data/contexts/ContextoUsuario'
 
 export default function Inicio() {
     const { produtos } = useProdutos()
@@ -25,23 +23,19 @@ export default function Inicio() {
     }
 
     return (
-        <ProvedorSessao>
-            <ProvedorUsuario>
-                <div className="container mx-auto px-4 py-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-                    {produtos.map((produto) => (
-                        <ProdutoItem key={produto.id} produto={produto} />
-                    ))}
+        <div className="container mx-auto px-4 py-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            {produtos.map((produto) => (
+                <ProdutoItem key={produto.id} produto={produto} />
+            ))}
 
-                    {showButton && (
-                        <button
-                            onClick={scrollToTop}
-                            className="fixed bottom-5 right-5 p-3 bg-purple-500 text-white rounded-full shadow-lg hover:bg-purple-800 transition"
-                        >
-                            <IconArrowNarrowUp size={18} />
-                        </button>
-                    )}
-                </div>
-            </ProvedorUsuario>
-        </ProvedorSessao>
+            {showButton && (
+                <button
+                    onClick={scrollToTop}
+                    className="fixed bottom-5 right-5 p-3 bg-purple-500 text-white rounded-full shadow-lg hover:bg-purple-800 transition"
+                >
+                    <IconArrowNarrowUp size={18} />
+                </button>
+            )}
+        </div>
     )
 }
